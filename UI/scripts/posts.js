d3978fd548e7606404adc2f233bfc713b58b0629
Gleb <raul221299@gmail.com>
2019-03-11 22:40:59 +0300

    add tests,almost done

--- UI/scripts/posts.js
+++ UI/scripts/posts.js
@@ -7,7 +7,7 @@ var posts = (function () {
             createdAt: new Date("2013-02-23T23:00:00"),
             author: "Mr.Snow",
             photoLink: "images/1",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -16,7 +16,7 @@ var posts = (function () {
             createdAt: new Date("2012-03-23T23:00:00"),
             author: "gleb9990",
             photoLink: "images/2",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -25,7 +25,7 @@ var posts = (function () {
             createdAt: new Date("2018-02-23T23:00:00"),
             author: "Mr.Snow",
             photoLink: "images/3",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -34,7 +34,7 @@ var posts = (function () {
             createdAt: new Date("2018-05-23T23:00:00"),
             author: "gleb9990",
             photoLink: "images/4",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -43,7 +43,7 @@ var posts = (function () {
             createdAt: new Date("2018-02-23T23:00:00"),
             author: "gleb9990",
             photoLink: "images/5",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -52,7 +52,7 @@ var posts = (function () {
             createdAt: new Date("2018-02-23T23:00:00"),
             author: "gleb9990",
             photoLink: "images/6",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -61,7 +61,7 @@ var posts = (function () {
             createdAt: new Date("2018-06-23T23:00:00"),
             author: "gleb9990",
             photoLink: "images/7",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -70,7 +70,7 @@ var posts = (function () {
             createdAt: new Date("2018-02-23T23:00:00"),
             author: "gleb9990",
             photoLink: "images/8",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -79,7 +79,7 @@ var posts = (function () {
             createdAt: new Date("2015-02-23T23:00:00"),
             author: "gleb9990",
             photoLink: "images/9",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -88,7 +88,7 @@ var posts = (function () {
             createdAt: new Date("2018-02-23T23:00:00"),
             author: "gleb9990",
             photoLink: "images/10",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -97,7 +97,7 @@ var posts = (function () {
             createdAt: new Date("2018-02-23T23:00:00"),
             author: "gleb9990",
             photoLink: "images/11",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -106,7 +106,7 @@ var posts = (function () {
             createdAt: new Date("2013-02-23T23:00:00"),
             author: "gleb9990",
             photoLink: "images/12",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -115,7 +115,7 @@ var posts = (function () {
             createdAt: new Date("2018-02-23T23:00:00"),
             author: "gleb9990",
             photoLink: "images/13",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -124,7 +124,7 @@ var posts = (function () {
             createdAt: new Date("2011-02-23T23:00:00"),
             author: "gleb9990",
             photoLink: "images/14",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -133,7 +133,7 @@ var posts = (function () {
             createdAt: new Date("2018-02-23T23:00:00"),
             author: "gleb9990",
             photoLink: "images/15",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -142,7 +142,7 @@ var posts = (function () {
             createdAt: new Date("2018-02-23T23:00:00"),
             author: "gleb9990",
             photoLink: "images/16",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -151,7 +151,7 @@ var posts = (function () {
             createdAt: new Date("2018-02-23T23:00:00"),
             author: "gleb9990",
             photoLink: "images/17",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
@@ -160,26 +160,26 @@ var posts = (function () {
             createdAt: new Date("2018-02-23T23:00:00"),
             author: "gleb9990",
             photoLink: "images/18",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
             id: "19",
             description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
             createdAt: new Date("2018-02-23T23:00:00"),
-            author: "gleb9990",
+            author: "glebek2h",
             photoLink: "images/19",
-            likes: ["gleb","hleb"],
+            likes: ["gleb", "hleb"],
             hashtags: ["#famcs", "#bsu", "#programming"],
         },
         {
             id: "20",
             description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
             createdAt: new Date("2018-02-23T23:00:00"),
-            author: "gleb9990",
+            author: "glebek2h",
             photoLink: "images/20",
-            likes: ["gleb","hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
+            likes: ["gleb", "hleb"],
+            hashtags: ["#fadwawd", "#bdaw", "#daw"],
         },
     ];
     var defaultFilterConfig =
@@ -190,47 +190,82 @@ var posts = (function () {
         hashtags: [],
     };
 
-    function commonHashtags(posthashtags, confighashtags) {
-        for (var i = 0; i < posthashtags.length; i++) {
-            for (var j = 0; j < confighashtags.length; j++) {
-                if (posthashtags[i] == confighashtags[j]) {
-                    return true;
+    function commonHashtags(postTags, configTags) {
+        var count = 0;
+        for (var i = 0; i < postTags.length; i++) {
+            for (var j = 0; j < configTags.length; j++) {
+                if (configTags[j] === postTags[i]) {
+                    count++;
                 }
             }
+        }
+        if (count !== 0 && count === configTags.length) {
+            return true;
         }
         return false;
     }
     module.getPhotoPosts = function (skip = 0, top = 10, filterConfig = defaultFilterConfig) {
         if (typeof skip !== 'number' || typeof top !== 'number' || typeof filterConfig !== 'object') {
-            console.log('Incorrect getPhotoPosts params');
             return false;
         }
-        var filtered_mas = photoPosts.filter(a =>
+        if (filterConfig !== defaultFilterConfig) {
+            if (!filterConfig.author) {
+                filterConfig.author = defaultFilterConfig.author;
+            } if (!filterConfig.dateFrom) {
+                filterConfig.dateFrom = defaultFilterConfig.dateFrom;
+            }
+            if (!filterConfig.dateTo) {
+                filterConfig.dateTo = defaultFilterConfig.dateTo;
+            }
+            if (!filterConfig.hashtags) {
+                filterConfig.hashtags = defaultFilterConfig.hashtags;
+            }
+        }
+        var filtered = photoPosts.filter(a =>
             (a.createdAt >= filterConfig.dateFrom || !filterConfig.dateFrom) &&
             (a.createdAt <= filterConfig.dateTo || !filterConfig.dateTo) &&
-            (a.author == filterConfig.author || filterConfig.author == "") &&
-            (commonHashtags(a.hashtags, filterConfig.hashtags) || filterConfig.hashtags.length == 0)
+            (a.author === filterConfig.author || filterConfig.author === "") &&
+            (commonHashtags(a.hashtags, filterConfig.hashtags) || filterConfig.hashtags.length === 0)
         ).sort(function (a, b) {
-            return a.createdAt - b.createdAt;
+            return b.createdAt - a.createdAt;
         }).slice(skip, skip + top);
-        return filtered_mas;
+        return filtered;
     }
 
-    module.getPhotoPost = function (id)//check
-    {
+    module.getPhotoPost = function (id) {
         return photoPosts.find((el) => el.id === id);
     };
-    module.validatePhotoPost = function (post) {
-        if (typeof (post.author) !== 'string' || typeof (post.description) !== 'string' ||
-            !post.createdAt || !post.author || !post.photoLink || typeof (post.photoLink) !== 'string' ||
-            !post.likes || typeof (post.id) !== 'string' || !(post.createdAt instanceof Date))
+    module.validatePhotoPost = function (post) {//check
+        if (!post.id) {
             return false;
+        }
+        if (!post.description) {
+            return false;
+        }
+        else if (post.description.length >= 200) {
+            return false;
+        }
+        if (!post.createdAt) {
+            return false;
+        }
+        if (!post.author) {
+            return false;
+        }
+        else if (post.author.length === 0) {
+            return false;
+        }
+        if (!post.photoLink) {
+            return false;
+        }
+        else if (post.photoLink.length === 0) {
+            return false;
+        }
         return true;
     };
     module.addPhotoPost = function (post) {
-        if (!photoPosts.find((el) => el.id === post.id) && validatePhotoPost(post)) {
+        if (!photoPosts.find((el) => el.id === post.id) && module.validatePhotoPost(post)) {
             photoPosts.push(post);
-            photoPosts.sort((a, b) => a.createdAt - b.createdAt);
+            photoPosts.sort((a, b) => b.createdAt - a.createdAt);
             return true;
         }
         return false;
@@ -238,12 +273,14 @@ var posts = (function () {
     module.editPhotoPost = function (id, edits) {
         const i = photoPosts.findIndex((el) => el.id === id);
         if (i === -1) {
-            console.log('No posts with such ID');
             return false;
         }
         var post = posts.getPhotoPost(id);
+        var validateResult = module.validatePhotoPost(post);
+        if (validateResult === false)
+            return false;
         for (var field in edits) {
-            if (field != "id" && field != "author" && field != "createdAt" && field != "likes") {
+            if (field !== "id" && field !== "author" && field !== "createdAt" && field !== "likes") {
                 post[field] = edits[field];
             }
         }
@@ -253,7 +290,6 @@ var posts = (function () {
     module.removePhotoPost = function (id) {
         const i = photoPosts.findIndex((el) => el.id === id);
         if (i === -1) {
-            console.log('No posts with such ID');
             return false;
         }
         photoPosts.splice(i, 1);
--- UI/scripts/test.js
+++ UI/scripts/test.js
@@ -4,41 +4,100 @@ var authorConfig = {
     author: "Mr.Snow",
     hashtags: [],
 };
-var unvalid = {
-    id: "29",
-    description: ".",
-    createdAt: new Date("2018-02-23T23:00:00"),
-    author: "dqwq",
-    hashtags: ["awd"],
-};
-var valid = {
-    id: "2",
-    description: ".",
-    createdAt: new Date("2018-02-23T23:00:00"),
-    author: "dwqqdw",
-    photoLink: 'dqwwd',
-    likes: 10,
-    hashtags: ["adw"],
-};
 var post = {
-    id: "10",
+    id: "322",
     description: "MMM",
     createdAt: new Date("2000-02-23T23:00:00"),
     author: "glebek2h",
     photoLink: "images/38",
-    likes: 100,
+    likes: ["gleb", "seva"],
     hashtags: ["#famcs", "#bsu", "#programming"],
 }
 function test() {
-    console.log(posts.getPhotoPost("1"));
-    console.log(posts.getPhotoPosts(0, 10));
-    console.log(posts.getPhotoPosts(0, 10, authorConfig));
-    console.log(posts.validatePhotoPost(unvalid));
+    console.log(posts.getPhotoPosts(0, 10, { author: "Mr.Snow", dateFrom: new Date("2015-02-23T23:00:00") }));
+    console.log("trying to add 'good' posts - successful:")
     posts.addPhotoPost(post);
-    console.log(posts.getPhotoPosts(0, 21));
-    posts.editPhotoPost("1", { photoLink: "http://google.com", hashtags: ["cats", "dogs"] });
-    posts.removePhotoPost("2");
-    console.log(posts.getPhotoPosts(0, 10));
-    console.log(posts.getPhotoPosts(0, 10, authorConfig));
+    console.log(posts.getPhotoPosts(0, 30));
+    posts.addPhotoPost({
+        id: "228",
+        description: "MMM",
+        createdAt: new Date("2019-03-11T23:22:00"),
+        author: "glebek2h",
+        photoLink: "images/132",
+        likes: ["glebyshek", "seva"],
+        hashtags: ["#famcs", "#bsu", "#programming"],
+    });
+    console.log(posts.getPhotoPosts(0, 30));
+    console.log("trying to add 'bad' posts - unsuccessful:")
+    posts.addPhotoPost({
+        id: "228",
+        description: "MMM",
+        createdAt: new Date("2019-03-11T23:22:00"),
+        photoLink: "images/132",
+        likes: ["glebyshek", "seva"],
+        hashtags: ["#famcs", "#bsu", "#programming"],
+    });
+
+    console.log(posts.getPhotoPosts(0, 30));
+    console.log("-getPhotoPosts");
+    console.log("10 posts:");
+    console.log(posts.getPhotoPosts());
+    console.log("3 posts start from the second:");
+    console.log(posts.getPhotoPosts(1, 3));
+    console.log("skip = 6 and default-argument for top:");
+    console.log(posts.getPhotoPosts(6));
+    console.log("posts after filtering:");
+    console.log(posts.getPhotoPosts(0, 10, {
+        author: "glebek2h",
+        hashtags: ["#famcs", "#bsu"],
+        startDate: new Date(2018, 0, 0),
+    }));
+
+    console.log("-getPhotoPost");
+    console.log("post with id 2:");
+    console.log(posts.getPhotoPost("2"));
+    console.log("post with id 100:");
+    console.log(posts.getPhotoPost("100"));
+    console.log("with invalid argument:");
+    console.log(posts.getPhotoPost(100));
+
+    console.log("-validatePhotoPost");
+    console.log("with valid");
+    console.log(posts.validatePhotoPost({
+        id: "228",
+        author: "glebushek",
+        description: "MMMM",
+        photoLink: "acdw",
+        createdAt: new Date("2018-01-01")
+    }));
+    console.log("with invalid");
+    console.log(posts.validatePhotoPost({
+        author: "glebushek",
+        photoLink: "acdw",
+        createdAt: new Date("2018-01-01")
+    }));
+
+    console.log("-editPhotoPost");
+    console.log("id=3 post before editing:");
+    console.log(posts.getPhotoPost("3"));
+    console.log("try to edit id=3 post:");
+    console.log(posts.editPhotoPost("3", {
+        description: "new description",
+        hashtags: ["new tag1", "new tag2"]
+    }));
+    console.log("id=3 post after editing:");
+    console.log(posts.getPhotoPost("3"));
+    console.log("with invalid argument:");
+    console.log(posts.editPhotoPost("213"));
+
+    console.log("-removePhotoPost");
+    console.log("with invalid argument:");
+    console.log(posts.removePhotoPost(""));
+    console.log("remove id=3 post");
+    console.log(posts.removePhotoPost("3"));
+    console.log("try to get id=3 post");
+    console.log(posts.getPhotoPost("3"));
+    console.log("all posts: ");
+    console.log(posts.getPhotoPosts(0, 30));
 }
 test();
