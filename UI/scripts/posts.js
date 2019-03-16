6c8bd606fbe930acc97f7878d1cbfa2c604318b0
Gleb <raul221299@gmail.com>
2019-03-17 02:59:37 +0300

    task5(classes,ESLint)

--- UI/scripts/posts.js
+++ UI/scripts/posts.js
@@ -1,276 +1,95 @@
 
 /**
- * Always store items ordered br date
+ * Always store items ordered by date
  */
-var posts = (function () {
-    var module = {};
-    var photoPosts = [
-        {
-            id: "1",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2013-02-23T23:00:00"),
-            author: "Mr.Snow",
-            photoLink: "images/1",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "2",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2012-03-23T23:00:00"),
-            author: "gleb9990",
-            photoLink: "images/2",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "3",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2018-02-23T23:00:00"),
-            author: "Mr.Snow",
-            photoLink: "images/3",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "4",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2018-05-23T23:00:00"),
-            author: "gleb9990",
-            photoLink: "images/4",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "5",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2018-02-23T23:00:00"),
-            author: "gleb9990",
-            photoLink: "images/5",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "6",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2018-02-23T23:00:00"),
-            author: "gleb9990",
-            photoLink: "images/6",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "7",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2018-06-23T23:00:00"),
-            author: "gleb9990",
-            photoLink: "images/7",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "8",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2018-02-23T23:00:00"),
-            author: "gleb9990",
-            photoLink: "images/8",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "8",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2015-02-23T23:00:00"),
-            author: "gleb9990",
-            photoLink: "images/9",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "10",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2018-02-23T23:00:00"),
-            author: "gleb9990",
-            photoLink: "images/10",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "11",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2018-02-23T23:00:00"),
-            author: "gleb9990",
-            photoLink: "images/11",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "12",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2013-02-23T23:00:00"),
-            author: "gleb9990",
-            photoLink: "images/12",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "13",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2018-02-23T23:00:00"),
-            author: "gleb9990",
-            photoLink: "images/13",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "14",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2011-02-23T23:00:00"),
-            author: "gleb9990",
-            photoLink: "images/14",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "15",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2018-02-23T23:00:00"),
-            author: "gleb9990",
-            photoLink: "images/15",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "16",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2018-02-23T23:00:00"),
-            author: "gleb9990",
-            photoLink: "images/16",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "17",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2018-02-23T23:00:00"),
-            author: "gleb9990",
-            photoLink: "images/17",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "18",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2018-02-23T23:00:00"),
-            author: "gleb9990",
-            photoLink: "images/18",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#famcs", "#bsu", "#programming"],
-        },
-        {
-            id: "19",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2018-02-23T23:00:00"),
-            author: "glebek2h",
-            photoLink: "images/19",
-            likes: ["gleb", "hleb"],
-            hashtags: ["da"],
-        },
-        {
-            id: "20",
-            description: "Herring sea chub Sevan trout sleeper shark, zebra shark chum salmon, mudminnow Atlantic silverside South American Lungfish European eel. Goldspotted killifish snubnose eel Sacramento splittail zingel",
-            createdAt: new Date("2018-02-23T23:00:00"),
-            author: "glebek2h",
-            photoLink: "images/20",
-            likes: ["gleb", "hleb"],
-            hashtags: ["#fadwawd", "#bdaw", "#daw"],
-        },
-    ];
-    var defaultFilterConfig =
-    {
-        dateFrom: new Date(-8640000000000000),
-        dateTo: new Date(8640000000000000),
-        author: "",
-        hashtags: [],
-    };
 
-    module.getPhotoPosts = function (skip = 0, top = 10, filterConfig = defaultFilterConfig) {
-        if (typeof skip !== 'number' || typeof top !== 'number' || typeof filterConfig !== 'object') {
-            return false;
-        }
-        filterConfig = Object.assign({}, defaultFilterConfig, filterConfig || {});
-        var filtered = photoPosts.filter(a =>
-            (a.createdAt >= filterConfig.dateFrom || !filterConfig.dateFrom) &&
-            (a.createdAt <= filterConfig.dateTo || !filterConfig.dateTo) &&
-            (a.author === filterConfig.author || filterConfig.author === "") &&
-            (filterConfig.hashtags.every(el => a.hashtags.includes(el)) || filterConfig.hashtags.length === 0)
-        ).sort(function (a, b) {
-            return b.createdAt - a.createdAt;
-        }).slice(skip, skip + top);
-        return filtered;
+const defaultFilterConfig = {
+  _dateFrom: new Date(-8640000000000000), _dateTo: new Date(8640000000000000), _author: '', _hashtags: [],
+};
+class PhotoPost {
+  constructor(id, description, createdAt, author, photolink, tags = [], likes = []) {
+    this._id = id;
+    this._description = description;
+    this._createdAt = new Date(createdAt);
+    this._author = author;
+    this._photolink = photolink;
+    this._hashtags = tags;
+    this._likes = likes;
+  }
+
+  validate() {
+    if (!this._id || !this._description || this._description.length >= 200 || !this._createdAt
+        || !this._author || this._author.length === 0 || !this._photolink
+        || this._photolink.length === 0) {
+      return false;
     }
+    return true;
+  }
+}
+class PhotoPosts {
+  constructor() {
+    this._photoPosts = [];
+  }
 
-    module.getPhotoPost = function (id) {
-        return photoPosts.find((el) => el.id === id);
-    };
-    module.validatePhotoPost = function (post) {
-        if (!post.id) {
-            return false;
-        }
-        if (!post.description) {
-            return false;
-        }
-        else if (post.description.length >= 200) {
-            return false;
-        }
-        if (!post.createdAt) {
-            return false;
-        }
-        if (!post.author) {
-            return false;
-        }
-        else if (post.author.length === 0) {
-            return false;
-        }
-        if (!post.photoLink) {
-            return false;
-        }
-        else if (post.photoLink.length === 0) {
-            return false;
-        }
-        return true;
-    };
-    module.addPhotoPost = function (post) {
-        if (!module.getPhotoPost(post.id) && module.validatePhotoPost(post)) {
-            photoPosts.push(post);
-            return true;
-        }
-        return false;
-    };
-    module.editPhotoPost = function (id, edits) {
-        var post = posts.getPhotoPost(id);
-        if (post !== undefined) {
-            var validateResult = module.validatePhotoPost(post);
-            if (validateResult === false)
-                return false;
-            for (var field in edits) {
-                if (edits.hasOwnProperty(field) && field !== "id" && field !== "author" && field !== "createdAt" && field !== "likes") {
-                    post[field] = edits[field];
-                }
-            }
-            return true;
-        }
-        else return false;
-    };
-    module.removePhotoPost = function (id) {
-        const i = photoPosts.findIndex((el) => el.id === id);
-        if (i === -1) {
-            return false;
-        }
-        photoPosts.splice(i, 1);
-        console.log(photoPosts);
-        return true;
-    };
+  getPhotoPosts(skip = 0, top = 10, filterConfig = defaultFilterConfig) {
+    if (typeof skip !== 'number' || typeof top !== 'number' || typeof filterConfig !== 'object') {
+      return false;
+    }
+    filterConfig = Object.assign({}, defaultFilterConfig, filterConfig || {});
+    let filtered = this._photoPosts
+      .filter(a => (new Date(a._createdAt) >= filterConfig._dateFrom || !filterConfig._dateFrom)
+        && (new Date(a._createdAt) <= filterConfig._dateTo || !filterConfig._dateTo)
+        && (a._author === filterConfig._author || filterConfig._author === '')
+        && (filterConfig._hashtags.length === 0
+        || filterConfig._hashtags.every(el => a._hashtags.includes(el))));
+    filtered = filtered.sort((a, b) => b._createdAt - a._createdAt).slice(skip, skip + top);
+    return filtered;
+  }
 
-    return module;
-})();
+  getPhotoPost(id) {
+    return this._photoPosts.find(el => el._id === id) || undefined;
+  }
+
+  addPhotoPost(post) {
+    if (!this.getPhotoPost(post._id) && post.validate()) {
+      this._photoPosts.push(post);
+      return true;
+    }
+    return false;
+  }
+
+  editPhotoPost(id, edits) {
+    const post = this.getPhotoPost(id);
+    if (post !== undefined && post.validate()) {
+      Object.keys(edits).forEach((field) => {
+        if (field !== '_id' && field !== '_author' && field !== '_createdAt' && field !== '_likes') {
+          post[field] = edits[field];
+        }
+      });
+      return true;
+    }
+    return false;
+  }
+
+  removePhotoPost(id) {
+    const i = this._photoPosts.findIndex(el => el._id === id);
+    if (i === -1) {
+      return false;
+    }
+    this._photoPosts.splice(i, 1);
+    return true;
+  }
+
+  addAll(postsArray) {
+    const badPosts = [];
+    postsArray.forEach((post) => {
+      const valid = this.addPhotoPost(post);
+      if (!valid) badPosts.push(post);
+    });
+    return badPosts;
+  }
+
+  clear() {
+    this._photoPosts = [];
+  }
+}
--- UI/scripts/test.js
+++ UI/scripts/test.js
@@ -1,103 +1,131 @@
-var authorConfig = {
-    dateFrom: new Date(-8640000000000000),
-    dateTo: new Date(8640000000000000),
-    author: "Mr.Snow",
-    hashtags: [],
-};
-var post = {
-    id: "322",
-    description: "MMM",
-    createdAt: new Date("2000-02-23T23:00:00"),
-    author: "glebek2h",
-    photoLink: "images/38",
-    likes: ["gleb", "seva"],
-    hashtags: ["#famcs", "#bsu", "#programming"],
-}
+
 function test() {
-    console.log(posts.getPhotoPosts(0, 10, { author: "Mr.Snow", dateFrom: new Date("2015-02-23T23:00:00") }));
-    console.log("trying to add 'good' posts - successful:")
-    posts.addPhotoPost(post);
-    console.log(posts.getPhotoPosts(0, 30));
-    posts.addPhotoPost({
-        id: "228",
-        description: "MMM",
-        createdAt: new Date("2019-03-11T23:22:00"),
-        author: "glebek2h",
-        photoLink: "images/132",
-        likes: ["glebyshek", "seva"],
-        hashtags: ["#famcs", "#bsu", "#programming"],
-    });
-    console.log(posts.getPhotoPosts(0, 30));
-    console.log("trying to add 'bad' posts - unsuccessful:")
-    posts.addPhotoPost({
-        id: "228",
-        description: "MMM",
-        createdAt: new Date("2019-03-11T23:22:00"),
-        photoLink: "images/132",
-        likes: ["glebyshek", "seva"],
-        hashtags: ["#famcs", "#bsu", "#programming"],
-    });
+  const Posts = new PhotoPosts();
 
-    console.log(posts.getPhotoPosts(0, 30));
-    console.log("-getPhotoPosts");
-    console.log("10 posts:");
-    console.log(posts.getPhotoPosts());
-    console.log("3 posts start from the second:");
-    console.log(posts.getPhotoPosts(1, 3));
-    console.log("skip = 6 and default-argument for top:");
-    console.log(posts.getPhotoPosts(6));
-    console.log("posts after filtering:");
-    console.log(posts.getPhotoPosts(0, 10, {
-        author: "glebek2h",
-        hashtags: ["#famcs", "#bsu"],
-        startDate: new Date(2018, 0, 0),
-    }));
+  Posts.addPhotoPost(new PhotoPost('1', '_DESCRIPTION', new Date('2011-02-23T23:00:00'), 'gleb9990', 'LINK', ['#famcs'], ['kek']));
+  Posts.addPhotoPost(new PhotoPost('2', '_DESCRIPTION', new Date('2000-02-23T23:00:00'), 'nastya', 'LINK', ['#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('3', '_DESCRIPTION', new Date('2018-02-23T23:00:00'), 'glebek2h', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('4', '_DESCRIPTION', new Date('2014-02-23T23:00:00'), 'gleb9990', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('5', '_DESCRIPTION', new Date('2018-02-23T23:00:00'), 'kop', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('6', '_DESCRIPTION', new Date('2012-02-23T23:00:00'), 'dima', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('7', '_DESCRIPTION', new Date('2007-02-23T23:00:00'), 'sasha', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('8', '_DESCRIPTION', new Date('2018-02-23T23:00:00'), 'gleb', 'LINK', ['#famcs', ' #bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('9', '_DESCRIPTION', new Date('2018-02-23T23:00:00'), 'gleb9990', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('10', '_DESCRIPTION', new Date('2018-02-23T23:00:00'), 'gleb', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('11', '_DESCRIPTION', new Date('2018-02-23T23:00:00'), 'bogdan', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('12', '_DESCRIPTION', new Date('2018-02-23T23:00:00'), 'glebek2h', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('13', '_DESCRIPTION', new Date('2008-02-23T23:00:00'), 'gleb9990', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('14', '_DESCRIPTION', new Date('2018-02-23T23:00:00'), 'gleb9990', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('15', '_DESCRIPTION', new Date('2001-02-23T23:00:00'), 'kira', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('16', '_DESCRIPTION', new Date('2018-02-23T23:00:00'), 'seva', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('17', '_DESCRIPTION', new Date('1999-02-23T23:00:00'), 'gleb9990', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('18', '_DESCRIPTION', new Date('2018-02-23T23:00:00'), 'nikita', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('19', '_DESCRIPTION', new Date('1963-02-23T23:00:00'), 'gleb9990', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
+  Posts.addPhotoPost(new PhotoPost('20', '_DESCRIPTION', new Date('2018-02-23T23:00:00'), 'glebyshek', 'LINK', ['#famcs', '#bsu'], ['lol', 'kek']));
 
-    console.log("-getPhotoPost");
-    console.log("post with id 2:");
-    console.log(posts.getPhotoPost("2"));
-    console.log("post with id 100:");
-    console.log(posts.getPhotoPost("100"));
-    console.log("with invalid argument:");
-    console.log(posts.getPhotoPost(100));
+  console.log("trying to add 'good' posts - successful:");
+  Posts.addPhotoPost(new PhotoPost('228', '_DESCRIPTION', new Date('2019-02-23T23:00:00'), 'hunter', 'LINK', ['#famcs', ' #bsu'], ['lol', 'kek']));
+  console.log(Posts.getPhotoPosts(0, 30));
+  Posts.addPhotoPost(new PhotoPost(
+    '322',
+    'MMM',
+    new Date('2006-02-23T23:00:00'),
+    'glebek2h',
+    'images/132',
+    ['#famcs', '#bsu', '#programming'],
+    ['glebyshek', 'seva'],
+  ));
+  console.log(Posts.getPhotoPosts(0, 30));
+  console.log("trying to add 'bad' posts - unsuccessful:");
+  Posts.addPhotoPost(new PhotoPost(
+    '228',
+    'MMM',
+    new Date('2019-03-11T23:22:00'),
+    'images/132',
+    ['glebyshek', 'seva'],
+    ['#famcs', '#bsu', '#programming'],
+  ));
 
-    console.log("-validatePhotoPost");
-    console.log("with valid");
-    console.log(posts.validatePhotoPost({
-        id: "228",
-        author: "glebushek",
-        description: "MMMM",
-        photoLink: "acdw",
-        createdAt: new Date("2018-01-01")
-    }));
-    console.log("with invalid");
-    console.log(posts.validatePhotoPost({
-        author: "glebushek",
-        photoLink: "acdw",
-        createdAt: new Date("2018-01-01")
-    }));
+  console.log(Posts.getPhotoPosts(0, 30));
+  console.log('-getPhotoPosts');
+  console.log('10 posts:');
+  console.log(Posts.getPhotoPosts());
+  console.log('3 posts start from the second:');
+  console.log(Posts.getPhotoPosts(1, 3));
+  console.log('skip = 6 and default-argument for top:');
+  console.log(Posts.getPhotoPosts(6));
+  console.log('posts after filtering:');
+  console.log(Posts.getPhotoPosts(0, 20, {
+    _author: 'gleb9990',
+    _hashtags: ['#famcs', '#bsu'],
+    _dateFrom: new Date('2010-02-23T23:00:00'),
+  }));
+  console.log(Posts.getPhotoPosts(0, 20, { _author: 'gleb9990' }));
 
-    console.log("-editPhotoPost");
-    console.log("id=3 post before editing:");
-    console.log(posts.getPhotoPost("3"));
-    console.log("try to edit id=3 post:");
-    console.log(posts.editPhotoPost("3", {
-        description: "new description",
-        hashtags: ["new tag1", "new tag2"]
-    }));
-    console.log("id=3 post after editing:");
-    console.log(posts.getPhotoPost("3"));
-    console.log("with invalid argument:");
-    console.log(posts.editPhotoPost("213"));
+  console.log('-getPhotoPost');
+  console.log('post with id 2:');
+  console.log(Posts.getPhotoPost('2'));
+  console.log('post with id 100:');
+  console.log(Posts.getPhotoPost('100'));
+  console.log('with invalid argument:');
+  console.log(Posts.getPhotoPost(100));
 
-    console.log("-removePhotoPost");
-    console.log("with invalid argument:");
-    console.log(posts.removePhotoPost(""));
-    console.log("remove id=3 post");
-    console.log(posts.removePhotoPost("3"));
-    console.log("try to get id=3 post");
-    console.log(posts.getPhotoPost("3"));
-    console.log("all posts: ");
-    console.log(posts.getPhotoPosts(0, 30));
+  console.log('-validate');
+  console.log('with valid');
+  console.log(new PhotoPost(
+    '228',
+    'glebushek',
+    'MMMM',
+    'acdw',
+    new Date('2018-01-01'),
+  ).validate());
+  console.log('with invalid');
+  console.log(new PhotoPost(
+    'glebushek',
+    'acdw',
+    new Date('2018-01-01'),
+  ).validate());
+
+  console.log('-editPhotoPost');
+  console.log('id=3 post before editing:');
+  console.log(Posts.getPhotoPost('3'));
+  console.log('try to edit id=3 post:');
+  console.log(Posts.editPhotoPost('3', {
+    _description: 'new _description',
+    _hashtags: ['new tag1', 'new tag2'],
+  }));
+  console.log('id=3 post after editing:');
+  console.log(Posts.getPhotoPost('3'));
+  console.log('with invalid argument:');
+  console.log(Posts.editPhotoPost('213'));
+
+  console.log('-removePhotoPost');
+  console.log('with invalid argument:');
+  console.log(Posts.removePhotoPost(''));
+  console.log('remove id=3 post');
+  console.log(Posts.removePhotoPost('3'));
+  console.log('try to get id=3 post');
+  console.log(Posts.getPhotoPost('3'));
+  console.log('all posts: ');
+  console.log(Posts.getPhotoPosts(0, 30));
+
+  console.log('-addAll');
+  console.log(Posts.addAll([
+    new PhotoPost('1000', 'des', new Date('2012-02-23T23:00:00'), 'me', 'link', ['#1', '#2'], ['gleb', 'hleb']),
+    new PhotoPost('1001', 'des', new Date('2012-02-23T23:00:00'), 'me', 'link', ['#1', '#2'], ['gleb', 'hleb']),
+    new PhotoPost('1002', 'des', new Date('2013-02-23T23:00:00'), 'me', 'link', ['#1', '#2'], ['gleb', 'hleb']),
+    new PhotoPost('1003', 'des', new Date('2014-02-23T23:00:00'), 'me', 'link', ['#1', '#2'], ['gleb', 'hleb']),
+    new PhotoPost('me', 'link', ['#1', '#2'], ['gleb', 'hleb']), // 'bad' post
+    new PhotoPost(['#1', '#2'], ['gleb', 'hleb']), // 'bad' post
+    new PhotoPost('1003', 'ivalid', new Date('2014-02-23T23:00:00'), 'me', 'link'), // 'bad' post
+    new PhotoPost('1004', 'des', new Date('2011-02-23T23:00:00'), 'me', 'link', ['#1', '#2'], ['gleb', 'hleb']),
+  ]));
+  console.log('all posts: ');
+  console.log(Posts.getPhotoPosts(0, 30));
+
+  console.log('-clear');
+  Posts.clear();
+  console.log('all posts: ');
+  console.log(Posts.getPhotoPosts(0, 30));
 }
 test();
